b39e9d369e95c36d77c055e10b27d7873f4a425e
Constant <constantsuchet@gmail.com>
2025-11-12 17:37:10 +0100

    docjercomposeezaeza

--- MessageX/backend/src/main.ts
+++ MessageX/backend/src/main.ts
@@ -7,8 +7,8 @@ import { initializeDatabase, closeDatabase } from './database';
 import usersRouter from './routes/users';
 import { seed } from './seed';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const host = process.env.HOST ?? '0.0.0.0';
+const port = process.env.PORT ? Number(process.env.PORT) : 80;
 
 const app = express();
 const server = createServer(app); // <-- Create the HTTP server
