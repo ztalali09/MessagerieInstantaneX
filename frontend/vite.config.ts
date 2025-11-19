3198f983d8601b64bf497e13b0105b22a68c4bf9
Constant <constantsuchet@gmail.com>
2025-11-19 07:02:21 +0100

    Added tailwindcss into the project, and removed a useless dockercompose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types='vitest' />
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(() => ({
   base: './',
@@ -14,7 +15,7 @@ export default defineConfig(() => ({
     port: 4300,
     host: 'localhost',
   },
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [],
